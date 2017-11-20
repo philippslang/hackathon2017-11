717d93cbf6c0d0e71e6a2f427b2e22760eeebf5c
plang85 <philipp.s.lang@gmail.com>
2017-11-20 22:12:56 +0000

    some more

--- tests/test.py
+++ tests/test.py
@@ -20,6 +20,8 @@ def test():
             f.write('\n')
             f.write('1,3%4%5,0%0%0,t4%t5%t6')
             f.write('\n')
+            f.write('1,3%4%5%6,0%0%1%0,t4%t5%t6%t7')
+            f.write('\n')
 
     tests = hackathon.tests.Tests(order=[])
     with hackathon.ci.CI(tests, FNAME) as ci:
@@ -28,6 +30,16 @@ def test():
             print(result)
             if i == 0:
                 assert result.score == 3
+                for test in result.test_results:
+                    if test.name == 't1':
+                        assert test.cost == 1
+                        assert test.status() == 'passed'
+                    if test.name == 't2':
+                        assert test.cost == 2
+                        assert test.status() == 'failed'
+                    if test.name == 't3':
+                        assert test.cost == 3
+                        assert test.status() == 'passed'
             if i == 1:
                 assert result.score == 3
             if i == 2:
@@ -37,9 +49,25 @@ def test():
                 assert result.score == 5
             if i == 4:
                 assert result.score == None
+                tests.order = ['t4', 't6', 'xx', 't7', 't5' ]
+            if i == 5:
+                assert result.score == 8
+                for test in result.test_results:
+                    if test.name == 't4':
+                        assert test.cost == 3
+                        assert test.status() == 'passed'
+                    if test.name == 't5':
+                        assert test.cost == 4
+                        assert test.status() == 'passed'
+                    if test.name == 't6':
+                        assert test.cost == 5
+                        assert test.status() == 'failed'
+                    if test.name == 't7':
+                        assert test.cost == 6
+                        assert test.status() == 'passed'
             i += 1
-        assert i == 5
-        assert ci.score() == 23
+        assert i == 6
+        assert ci.score() == 31
         print(ci)
     
     os.remove(FNAME)
